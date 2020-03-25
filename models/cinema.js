233828eb289518f7e5c9c9088d236bb6c86d9664
michaeld55 <michaeld55@hotmail.co.uk>
2020-03-25 15:29:17 +0000

    end of lab

---
+++ models/cinema.js
@@ -0,0 +1,63 @@
+const Cinema = function (films) {
+  this.films = films;
+};
+
+Cinema.prototype.titleList = function(){
+  result = []
+  result = this.films.map(function(film){
+    return film.title;
+  });
+  return result
+};
+
+Cinema.prototype.findTitle = function(title){
+  result = this.films.find(film => film.title === title);
+  
+  return result;
+};
+
+Cinema.prototype.filterGenre = function(genre){
+  result = this.films.filter(film => film.genre === genre);
+  return result;
+}
+
+Cinema.prototype.particularYear = function(year){
+  result = this.films.some(film => film.year === year)
+  return result;
+};
+
+Cinema.prototype.filmOverLength = function(length){
+  result = this.films.every(film => film.length > length)
+  return result;
+};
+
+Cinema.prototype.totalRunTime = function(){
+  times = []
+  times = this.films.map(function(film){
+    return film.length;
+  });
+  result = times.reduce((total, time) => total + time);
+  return result;
+
+};
+
+Cinema.prototype.filterByProperty = function(property, value){
+  console.log(property)
+  result = this.films.filter((film.property) => (film.property === value)); //&& film.property === property));
+  
+  return result;
+
+  //result = this.films.filter(callback(property[, index, [films]])[, film])
+}
+module.exports = Cinema;
+
+//Cinema:
+// Add a another test, `'Cinema should be able to filter films by year'`.
+
+// We already have a method that filters films by genre, the functionality of which is very similar. We don't want two separate methods as that wouldn't be DRY. Your task is get the final test to pass by to writing a new method called `filmsByProperty`, which takes two arguments:
+
+// 1. the name of the property
+// 2. the value being search for
+
+// Once the final test is passing, modify the test `'Cinema should be able to filter films by genre'` to use the new `filmsByProperty` method.
+//If you use reduce, remember that you will need to pass in the initial value of the accumulator as the second argument.
---
+++ specs/cinema_spec.js
@@ -0,0 +1,78 @@
+const assert = require('assert');
+const Cinema = require('../models/cinema.js');
+const Film = require('../models/film.js');
+
+describe('Cinema', function () {
+
+  let moonlight;
+  let bladeRunner;
+  let dunkirk;
+  let blackPanther;
+  let trainspotting;
+  let films;
+  let cinema;
+
+  beforeEach(function () {
+    moonlight = new Film('Moonlight', 'drama', 2016, 111);
+    bladeRunner = new Film('Blade Runner 2049', 'sci-fi', 2017, 164);
+    dunkirk = new Film('Dunkirk', 'history', 2017, 96);
+    blackPanther = new Film('Black Panther', 'action', 2018, 134);
+    trainspotting = new Film('T2 Trainspotting', 'drama', 2017, 117);
+
+    films = [moonlight, bladeRunner, dunkirk, blackPanther, trainspotting];
+    cinema = new Cinema(films);
+  });
+
+  it('should have a collection of films', function () {
+    const actual = cinema.films;
+    assert.deepStrictEqual(actual, films);
+  });
+
+  it('should be able to get a list of film titles',function(){
+    const actual = cinema.titleList();
+    const expected = ["Moonlight", "Blade Runner 2049", "Dunkirk", "Black Panther", "T2 Trainspotting"]
+    assert.deepStrictEqual(actual, expected)
+  });
+  
+  it('should be able to find a film by title',function(){
+    const actual = cinema.findTitle("Moonlight");
+    assert.deepStrictEqual(actual, moonlight)  
+  });
+
+  it('should be able to filter films by genre',function(){
+    const actual = cinema.filterGenre("drama");
+    const expected = [moonlight, trainspotting];
+    assert.deepStrictEqual(actual, expected);
+  });
+
+  it('should be able to check whether there are some films from a particular year',function(){
+    const actual = cinema.particularYear(2017);
+    assert.strictEqual(actual, true);
+  });
+
+  it('should be able to check whether there are no films from a particular year',function(){
+    const actual = cinema.particularYear(2020);
+    assert.strictEqual(actual, false);
+  });
+
+  it('should be able to check whether all films are over a particular length',function(){
+    const actual = cinema.filmOverLength(90);
+    assert.strictEqual(actual, true);
+    const actual2 = cinema.filmOverLength(100);
+    assert.strictEqual(actual2, false);
+  });
+
+  it('should be able to calculate total running time of all films',function(){
+    const actual = cinema.totalRunTime();
+    assert.strictEqual(actual, 622)
+  });
+
+  it('should be able to filter films by property',function(){
+    const actual = cinema.filterByProperty("title", "Moonlight");
+    const expected = [moonlight];
+    assert.deepStrictEqual(actual, expected);
+  });
+
+  // Add a another test, `'Cinema should be able to filter films by year'`.
+
+});
